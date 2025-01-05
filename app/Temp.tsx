c3b2e8472d62355c639ffec5a292b94d5a7ce55b
Soham Karandikar <sohamk10@gmail.com>
2025-01-05 15:29:01 +0530

    Set realm and client details from env variables

--- app/Temp.tsx
+++ app/Temp.tsx
@@ -1,9 +1,19 @@
 "use client";
 import { useEffect, useState } from "react";
+import { authClient } from "@auth/client";
 
 export default function Temp() {
+  const signInTest = async () => {
+    const response = await authClient.signIn.oauth2({
+      providerId: "keycloak",
+      callbackURL: "/dashboard",
+    });
+    console.log(response);
+  };
+
   useEffect(() => {
     console.log("hi");
+    signInTest();
   });
 
   return <div>Temp</div>;
