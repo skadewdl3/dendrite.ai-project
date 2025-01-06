f2e687c69479a9864c8547b4ec23cd282fc5b4f7
Soham Karandikar <sohamk10@gmail.com>
2025-01-06 23:50:47 +0530

    Implemented realtime sync of clipboard

--- lib/fabric/init.ts
+++ lib/fabric/init.ts
@@ -1,4 +1,4 @@
-import { Canvas, Rect } from "fabric";
+import { Canvas, Path, Rect, util } from "fabric";
 
 export const setupCanvas = (
   canvasElement: HTMLCanvasElement,
@@ -13,7 +13,9 @@ export const setupCanvas = (
 };
 
 export const setupCanvasListeners = (canvas: Canvas, socket: WebSocket) => {
+  let stopEvents = false;
   canvas.on("object:added", (e) => {
+    if (stopEvents) return;
     socket.send(
       JSON.stringify({
         type: "canvas",
@@ -21,5 +23,22 @@ export const setupCanvasListeners = (canvas: Canvas, socket: WebSocket) => {
       }),
     );
     console.log(e);
+  });
+
+  socket.addEventListener("message", async (event) => {
+    const message =
+      typeof event.data === "string" ? event.data : await event.data.text();
+
+    const { type, data: changes } = JSON.parse(message);
+    if (type != "canvas") return;
+
+    // const path = new Path(changes);
+    const objects = await util.enlivenObjects([changes]);
+    stopEvents = true;
+    objects.forEach((obj) => canvas.add(obj));
+    canvas.renderAll();
+    stopEvents = false;
+
+    console.log("changes: ", changes);
   });
 };
